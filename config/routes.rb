5ce40f97db11a90b9c6deca4ade6ad7344af274e
Dev Halai <dev.h@hotmail.co.uk>
2024-09-10 12:22:37 +0100

    card provider template

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,7 @@ Rails.application.routes.draw do
 
   # static pages
   pages = %w[
-    privacy terms about contact home home2 home3 home4 guides calculator faq
+    privacy terms about contact home home2 home3 home4 guides calculator faq test
   ]
 
   pages.each do |page|
