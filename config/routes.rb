ff66770f97f541c72f147e8f30c90b75a4928f4e
Dev Halai <dev.h@hotmail.co.uk>
2024-09-12 11:15:38 +0100

    env setup

--- config/routes.rb
+++ config/routes.rb
@@ -35,7 +35,7 @@ Rails.application.routes.draw do
 
   # static pages
   pages = %w[
-    privacy terms about contact home home2 home3 home4 guides calculator faq pricing test
+    privacy terms about contact home home2 home3 home4 guides calculator faq pricing search test
   ]
 
   pages.each do |page|
