d4147e31c9478965018d617f1309ef6029c6fd95
Dev Halai <dev.h@hotmail.co.uk>
2024-09-20 00:22:02 +0100

    dashboard edit

--- app/models/booking_enquiry.rb
+++ app/models/booking_enquiry.rb
@@ -1,4 +1,9 @@
 class BookingEnquiry < ApplicationRecord
   belongs_to :user
   belongs_to :room
+
+  # Each time a booking is made, it will stream it to the activity_feed turbo stream.
+  # after_create_commit do
+  #   broadcast_append_to 'activity_feed', target: 'activity-feed', partial: 'activity_feeds/card', locals: { booking: self }
+  # end
 end
