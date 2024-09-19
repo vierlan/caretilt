9597114611b209728ef1c7bbee0446075e7dd415
Lananh Nguyen <lananhnguyen@live.co.uk>
2024-09-19 14:09:01 +0100

    admin panel added

--- app/javascript/controllers/togglevisibility_controller.js
+++ app/javascript/controllers/togglevisibility_controller.js
@@ -11,15 +11,16 @@ export default class extends Controller {
   toggle(event) {
     const targetId = event.currentTarget.getAttribute("aria-controls");
     const content = document.getElementById(targetId);
-    
+
     content.classList.toggle("hidden");
-    
+
     const iconOpen = event.currentTarget.querySelector("[data-toggle-target='iconOpen']");
     const iconClosed = event.currentTarget.querySelector("[data-toggle-target='iconClosed']");
-    
+
     if (iconOpen && iconClosed) {
       iconOpen.classList.toggle("hidden");
       iconClosed.classList.toggle("hidden");
     }
   }
+  
 }
