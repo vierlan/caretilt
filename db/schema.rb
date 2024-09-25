29324058990482cd4ad60b0930c2c4639ff9df95
Dev Halai <dev.h@hotmail.co.uk>
2024-09-25 14:52:41 +0100

    validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_24_132413) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_24_182346) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,13 +63,14 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_24_132413) do
 
   create_table "booking_enquiries", force: :cascade do |t|
     t.string "contact_name"
-    t.string "contact_phone"
-    t.string "service_user_name"
+    t.string "phone_number"
+    t.string "reference_name"
     t.text "message"
     t.bigint "user_id", null: false
     t.bigint "room_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
     t.index ["room_id"], name: "index_booking_enquiries_on_room_id"
     t.index ["user_id"], name: "index_booking_enquiries_on_user_id"
   end
@@ -110,8 +111,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_24_132413) do
     t.string "billing_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "registration_pin", default: "2101", null: false
-    t.string "super_pin", default: "2399", null: false
+    t.string "registration_pin", default: "8415", null: false
+    t.string "super_pin", default: "9560", null: false
     t.string "stripe_customer_id"
     t.string "email"
     t.string "contact_name"
