c00f8a6af386daed998fee9783e490568ff9cd5a
Dev Halai <dev.h@hotmail.co.uk>
2024-10-01 20:27:14 +0100

    tailwind asset test

--- config/application.rb
+++ config/application.rb
@@ -28,13 +28,13 @@ module Caretilt
     config.active_job.queue_adapter = :delayed
 
     # mailers via postmark
-    config.action_mailer.default_url_options = { host: Rails.application.credentials.base_url }
+    config.action_mailer.default_url_options = { host: ENV['BASE_URL'] }
     config.action_mailer.default_options = { from: Rails.application.credentials.admin_email }
     config.action_mailer.delivery_method = :postmark
     config.action_mailer.postmark_settings = { api_token: Rails.application.credentials.postmark_api_token }
 
     # serve images from asset pipeline in mailers
-    config.asset_host = Rails.application.credentials.base_url
+    config.asset_host = ENV['BASE_URL']
 
     # customize generators
     config.generators do |g|
