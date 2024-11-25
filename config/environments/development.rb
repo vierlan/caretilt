aae6fc3ec005a12e5f2301815d7d0ec251e9cf8b
Lananh Nguyen <lananhnguyen@live.co.uk>
2024-11-25 15:58:53 +0000

    added lookup key to stripe package create

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,8 +49,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.hostinger.com",
     port: 587,
-    user_name: Rails.application.credentials.gmail.address,
-    password: Rails.application.credentials.gmail.password,
+    user_name: Rails.application.credentials.gmail[:address],
+    password: Rails.application.credentials.gmail[:password],
     authentication: "plain",
     enable_starttls_auto: true
   }
